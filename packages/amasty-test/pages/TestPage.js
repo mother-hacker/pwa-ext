9a0a877f3db97d72c916b04c9acccb94104eaccf
Dzmitry Shitik <dzmitry@Dzmitrys-MacBook-Pro.local>
2019-12-19 06:25:13 +0300

    test page

---
+++ packages/amasty-test/pages/TestPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class TestPage extends React.Component {
+    render() {
+        return <h1>Test Page!</h1>
+    }
+}
--- packages/venia-concept/webpack.config.js
+++ packages/venia-concept/webpack.config.js
@@ -45,7 +45,11 @@ module.exports = async env => {
                 graphqlQueries: true,
                 rootComponents: true,
                 upward: true
-            }
+            },
+            '@dshitik/amasty-test': {
+                esModules: true,
+                cssModules: true
+            },
         },
         env
     });
